8ef87562c883b6e7421b70457b0d774314f3b7bb
Ardison-Gardner <greg.ardison.gardner@gmail.com>
2019-03-11 09:44:26 -0400

    Submission for deadline.

---
+++ Assets/Scripts/Rail/Node.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+//using UnityEditor;
+
+public class Node : MonoBehaviour 
+{
+    public float duration;
+
+	void Start () 
+    {
+        duration = 1;
+	}
+	
+//    private void OnDrawGizmos()
+//    {
+//        Handles.ArrowCap(0, transform.position, transform.rotation, 3.0f);
+//    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# Unity-Checker-Multiplayer
+
+Created by Gregory Ardison-Gardner for Fullstack Academy's stackathon, cohort 1901-FSA-NY.
+
+Made with guidance from N3X EN and the Unity Docs:
+
+https://www.youtube.com/playlist?list=PLLH3mUGkfFCVXrGLRxfhst7pffE9o2SQO
